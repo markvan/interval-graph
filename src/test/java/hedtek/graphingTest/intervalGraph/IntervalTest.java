05bf3aa0762133501f5abf42ecf2e1b2fceec56b
markvan <markvanharmelen@gmail.com>
2023-10-30 10:38:37 +0000

    removed unnessary public for test class to adopt JUnit style

--- src/test/java/hedtek/graphingTest/intervalGraph/IntervalTest.java
+++ src/test/java/hedtek/graphingTest/intervalGraph/IntervalTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.catchThrowable;
 
-public class IntervalTest {
+class IntervalTest {
 
     @Test
     void intervalsShouldOverlap () {
